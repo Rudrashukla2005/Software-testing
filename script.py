8ecf396cecb7079a46bb605d56fba631eb56f797
Rudrashukla2005 <shuklarudra2005@gmail.com>
2026-08-30 23:14:59 +0530

    changes Done

--- script.py
+++ script.py
@@ -3,3 +3,6 @@ def test_addition():
     print("Test Passed: 2 + 2 == 4")
 if __name__ == "__main__":
     test_addition()
+def test_subtraction():
+    assert 5 - 2 == 3
+    print("Test Passed: 5 - 2 == 3")
